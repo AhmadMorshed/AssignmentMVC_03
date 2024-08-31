170a5c2c1775f26ce72c6e577a7376be9512d77e
AhmadMorshed <ahmadnakawa676@gmail.com>
2024-08-31 03:08:38 -0700

    first commit

---
+++ Company.Repository/Repositories/GenericRepository.cs
@@ -0,0 +1,35 @@
+﻿using Company.Data.Context;
+using Company.Data.Entities;
+using Company.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Repository.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly CompanyDBContext _context;
+        public GenericRepository( CompanyDBContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(T entity)
+        => _context.Set<T>().Add(entity);
+
+        public void Delete(T entity)
+        => _context.Set<T>().Remove(entity);
+
+        public IEnumerable<T> GetAll()
+        => _context.Set<T>().ToList();
+
+        public T GetById(int id)
+        =>_context.Set<T>().Find(id);
+
+        public void Update(T entity)
+        => _context.Set<T>().Update(entity);
+    }
+}
